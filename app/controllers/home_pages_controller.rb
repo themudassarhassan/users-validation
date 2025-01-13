2e344bd71c193ef284290db40d3ff7d79e6b579f
Mudassar Hassan <mudassarhassan135@gmail.com>
2025-01-13 14:57:51 +0500

    Add request specs for create action and handle malformed file case

--- app/controllers/home_pages_controller.rb
+++ app/controllers/home_pages_controller.rb
@@ -1,4 +1,6 @@
 class HomePagesController < ApplicationController
+  rescue_from Users::CSVImportService::CSVParsingError, with: :redirect_and_show_message
+
   def index
   end
 
@@ -15,5 +17,9 @@ class HomePagesController < ApplicationController
 
   def users_csv
      params[:users_csv]
+  end
+
+  def redirect_and_show_message(exception)
+    redirect_to root_url, alert: exception.message
   end
 end
--- spec/requests/home_page_spec.rb
+++ spec/requests/home_page_spec.rb
@@ -14,4 +14,28 @@ RSpec.describe "HomePages", type: :request do
       expect(response.body).to include('Upload users CSV file for validation.')
     end
   end
+
+  describe "POST /home_pages" do
+    let(:csv_content) { "name,password\nJohn Doe,password123\nJane Doe,password456" }
+    let(:csv_file) { Rack::Test::UploadedFile.new(StringIO.new(csv_content), 'text/csv', original_filename: 'users.csv') }
+
+    context 'with valid csv' do
+      it "returns turbo stream response" do
+        post home_page_path, params: { users_csv: csv_file }, headers: { accept: "text/vnd.turbo-stream.html" }
+
+        expect(response.media_type).to eq "text/vnd.turbo-stream.html"
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'with malformed csv file' do
+      let(:csv_content) { "name,password\n\"John\" Doe, pass,word" }
+
+      it "flashes an alert" do
+        post home_page_path, params: { users_csv: csv_file }, headers: { accept: "text/vnd.turbo-stream.html" }
+
+        expect(flash[:alert]).to match(/Invalid CSV format:/)
+      end
+    end
+  end
 end
